4d6a2579ed4fd9f1bf62d5cf677ffed6897621bc
DanielleZror <daniellezror@gmail.com>
2020-04-18 20:27:02 +0300

    Updates

---
+++ precache-manifest.7e1fd6ffb85e0e6c34424ebd09848ce7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3771bab31ba6a9bc32c766d13ec54fcd",
+    "url": "/myRecipesReactApp/index.html"
+  },
+  {
+    "revision": "390f796742e9bf4c19ba",
+    "url": "/myRecipesReactApp/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "c27622e1492b75ff0023",
+    "url": "/myRecipesReactApp/static/css/main.91a94fcb.chunk.css"
+  },
+  {
+    "revision": "390f796742e9bf4c19ba",
+    "url": "/myRecipesReactApp/static/js/2.719ccf2e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/myRecipesReactApp/static/js/2.719ccf2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c27622e1492b75ff0023",
+    "url": "/myRecipesReactApp/static/js/main.c2f91ab9.chunk.js"
+  },
+  {
+    "revision": "f3e0dda422e4504e45d9",
+    "url": "/myRecipesReactApp/static/js/runtime-main.ae87e188.js"
+  },
+  {
+    "revision": "a4c4175ddf30fbd3807ce4a69516723e",
+    "url": "/myRecipesReactApp/static/media/food1.a4c4175d.jpg"
+  },
+  {
+    "revision": "ccb1f31292871bb0707257479ffd0994",
+    "url": "/myRecipesReactApp/static/media/logo.ccb1f312.png"
+  }
+]);
