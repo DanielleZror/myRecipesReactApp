64d9ae57aa82719cf22cca05bfdbdf6fe6821bf1
DanielleZror <daniellezror@gmail.com>
2020-07-28 18:01:55 +0300

    server

---
+++ src/server/Api.js
@@ -0,0 +1,114 @@
+const { ObjectId } = require('mongodb');
+const GLOBAL = require('../constants')
+const QUERY = require('./Queries')
+const DB = require('./DB')
+
+module.exports = {
+    initApi: (app) => {
+        app.get('/api/recipe/popularRecipes', function (req, res) {
+            let query = QUERY.createPopularQuery(req.query.userID)
+            DB.selectWithJoinFromDB(sendRes, query, GLOBAL.RECIPES_COLLECTION)
+            function sendRes(result) {
+                res.send(result);
+            }
+        })
+
+        app.get('/api/recipe/allByUser', function (req, res) {
+            let match = { $match: { userID: req.query.userID } };
+            let query = QUERY.createJoinQuery(match, req.query.userID)
+            DB.selectWithJoinFromDB(sendRes, query, GLOBAL.RECIPES_COLLECTION)
+            function sendRes(result) {
+                res.send(result);
+                res.status(200).end()
+            }
+        })
+
+        app.get('/api/recipe/allSavedByUser', function (req, res) {
+            let query = QUERY.createSavedQuery(req.query.userID)
+            DB.selectWithJoinFromDB(sendRes, query, GLOBAL.RECIPES_COLLECTION)
+            function sendRes(result) {
+                res.send(result);
+                res.status(200).end()
+            }
+        })
+
+        app.get('/api/recipe/allRecipes', function (req, res) {
+            let match = { $match: {} }
+            let query = QUERY.createJoinQuery(match, req.query.userID);
+            DB.selectWithJoinFromDB(sendRes, query, GLOBAL.RECIPES_COLLECTION)
+            function sendRes(result) {
+                res.send(result);
+            }
+        })
+
+        app.get('/api/search', function (req, res) {
+            var regex = new RegExp("." + req.query.search + ".");
+            var match = {
+                $match: {
+                    $and: [
+                        {
+                            $or: [
+                                { Description: regex },
+                                { Item: regex },
+                                { Name: regex },
+                                { Preparation: regex }
+                            ]
+                        },
+                        { userID: req.query.userID }
+                    ]
+                }
+            }
+
+            let query = QUERY.createJoinQuery(match, req.query.userID);
+            DB.selectWithJoinFromDB(sendRes, query, GLOBAL.RECIPES_COLLECTION);
+            function sendRes(result) {
+                res.send(result);
+            }
+        })
+
+        app.get('/api/recipe/recipeByID', function (req, res) {
+            let match = { $match: { _id: ObjectId(req.query.id) } }
+            let query = QUERY.createJoinQuery(match, req.query.userID)
+            DB.selectWithJoinFromDB(sendRes, query, GLOBAL.RECIPES_COLLECTION);
+            function sendRes(result) {
+                res.send(result[0]);
+            }
+        })
+
+        app.post('/api/recipe/add', function (req, res) {
+            DB.addToDB(sendRes, req.body.recipe, GLOBAL.RECIPES_COLLECTION)
+            function sendRes(insertID) {
+                res.send(insertID)
+                res.status(200).end()
+            }
+        })
+
+        app.post('/api/recipe/likeRecipe', function (req, res) {
+            let query = { userID: req.body.like.userID, recipeID: ObjectId(req.body.like.recipeID) }
+            DB.addToDB(sendRes, query, GLOBAL.SAVED_COLLECTION)
+            function sendRes(insertID) {
+                res.send(insertID)
+                res.status(200).end()
+            }
+        })
+
+        app.post('/api/recipe/unlikeRecipe', function (req, res) {
+            let recipeID = ObjectId(req.body.unlike.recipeID)
+            let query = { $and: [{ userID: req.body.unlike.userID }, { recipeID: recipeID }] }
+            DB.RemoveFromDB(sendRes, query, GLOBAL.SAVED_COLLECTION)
+            function sendRes(result) {
+                res.send(result)
+                res.status(200).end()
+            }
+        })
+
+        app.post('/api/user/addUser', function (req, res) {
+            DB.createOrUpdate(sendRes, req.body.user, GLOBAL.USERS_COLLECTION)
+            function sendRes(result) {
+                res.send(result)
+                res.status(200).end()
+            }
+        })
+
+    }
+}
---
+++ src/server/DB.js
@@ -0,0 +1,104 @@
+var MongoClient = require('mongodb').MongoClient;
+var uri = 'mongodb://localhost:27017/recipes';
+let mydb
+let countMydb = 0
+
+function connectToDB(callback, collectionName) {
+    if (!mydb || !mydb.isConnected()) {
+        MongoClient.connect(uri, function (err, db) {
+            if (!err) {
+                if (!mydb) {
+                    mydb = db
+                    console.log("connected");
+                }
+                createCollection(callback, db, collectionName)
+            }
+        })
+    } else {
+        createCollection(callback, mydb, collectionName)
+    }
+}
+
+function createCollection(callback, db, collectionName) {
+    dbo = db.db("recipes");
+    countMydb++
+    callback(dbo.collection(collectionName))
+}
+
+function closeConnction() {
+    if (mydb) {
+        if (countMydb > 0) {
+            countMydb--
+        }
+        if (countMydb === 0) {
+            console.log("close")
+            mydb.close();
+            MongoClient.close;
+        }
+    }
+}
+module.exports = {
+
+    selectFromDB: (callback, query, collectionName) => {
+        connectToDB(find, collectionName)
+        function find(collection) {
+            collection.find(query).sort({ Date: -1 }).toArray(function (err, result) {
+                if (err) throw err;
+                callback(result)
+                closeConnction()
+            })
+        }
+    },
+
+    addToDB: (callback, document, collectionName) => {
+        connectToDB(insert, collectionName)
+        function insert(collection) {
+            document.Date = new Date().toJSON()
+            collection.insertOne(document, function (err, result) {
+                if (err) throw err;
+                callback(result.insertedId.toString())
+                closeConnction()
+            })
+        }
+    },
+
+    createOrUpdate: (callback, user, collectionName) => {
+        connectToDB(findAndModify, collectionName)
+        function findAndModify(collection) {
+            collection.findAndModify(
+                { userID: user.userID },
+                [['_id', 'asc']],
+                { $setOnInsert: { Name: user.Name, Email: user.Email } },
+                { new: true, upsert: true },
+                function (err, result) {
+                    if (err) throw err;
+                    callback(result)
+                    closeConnction()
+                }
+            )
+        }
+    },
+
+    selectWithJoinFromDB: (callback, query, collectionName) => {
+        connectToDB(join, collectionName)
+        function join(collection) {
+            collection.aggregate(query).sort({ Date: -1 }).toArray(function (err, result) {
+                if (err) throw err;
+                callback(result)
+                closeConnction()
+            })
+        }
+    },
+
+    RemoveFromDB: (callback, query, collectionName) => {
+        connectToDB(remove, collectionName)
+        function remove(collection) {
+            collection.remove(query, function (err, result) {
+                if (err) throw err;
+                callback(result)
+                closeConnction()
+            })
+        }
+    }
+
+}
